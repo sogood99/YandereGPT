23a34364d8c69aef9e2f3d6664cfee7f034f4b87
Onii Chan <46487694+sogood99@users.noreply.github.com>
2023-03-19 02:08:28 +0800

    finish download gpt2

---
+++ download_gpt2.py
@@ -0,0 +1,81 @@
+import os
+import configargparse
+import pathlib
+import time
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, set_seed
+
+
+def get_argparse():
+    p = configargparse.ArgParser(default_config_files=['./.my.config'])
+    p.add('-c', '--my-config', is_config_file=True, help='config file path')
+    p.add('--model_name', type=str, default="gpt2", help='model name')
+    p.add('--model_dir', type=pathlib.Path,
+          default="./models/gpt2/", help='path to genome file')
+    p.add('-v', help='verbose', action='store_true')
+    # this option can be set in a config file because it starts with '--'
+
+    options = p.parse_args()
+
+    if options.v:
+        print("----------")
+        print(p.format_help())
+        print("----------")
+        # useful for logging where different settings came from
+        print(p.format_values())
+    return options
+
+
+def main():
+    set_seed(42)
+
+    options = get_argparse()
+
+    if not os.path.exists(options.model_dir) or not os.listdir(options.model_dir):
+        # empty
+        print("Directory is empty")
+        tokenizer = GPT2Tokenizer.from_pretrained(options.model_name)
+        model = GPT2LMHeadModel.from_pretrained(options.model_name)
+
+        if type(model) is not GPT2LMHeadModel:
+            return
+
+        os.makedirs(options.model_dir, exist_ok=True)
+        tokenizer.save_pretrained(options.model_dir)
+        model.save_pretrained(options.model_dir)
+    else:
+        print("Directory is not empty, testing")
+
+    tokenizer = GPT2Tokenizer.from_pretrained(options.model_dir)
+    model = GPT2LMHeadModel.from_pretrained(options.model_dir)
+
+    if type(model) is not GPT2LMHeadModel:
+        return
+
+    # test it out
+    start_time = time.time()
+
+    text = "Replace me by any text you'd like. Or not"
+    print("\nInference: {}\n".format(text) + 100*'-')
+    encoded_input = tokenizer(text, return_tensors='pt')
+    sample_outputs = model.generate(
+        **encoded_input,
+        do_sample=True,
+        max_length=50,
+        top_k=50,
+        top_p=0.95,
+        num_return_sequences=1,
+        pad_token_id=tokenizer.eos_token_id
+    )
+
+    time_used = time.time() - start_time
+
+    print("Output:\n" + 100 * '-')
+    for i, sample_output in enumerate(sample_outputs):
+        print("{}: {}".format(i, tokenizer.decode(
+            sample_output, skip_special_tokens=True)))
+
+    print("Time Used: {:.3f}s".format(time_used))
+
+
+if __name__ == "__main__":
+    main()
